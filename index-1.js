244924ea3e901b24128bf518c9e5ce284994a85e
rodsen <1255339772@qq.com>
2019-07-31 01:51:04 +0800

    add Third clock

--- index-1.js
+++ index-1.js
@@ -3,12 +3,17 @@ window.onload = function () {
     var hour = document.getElementById("hour");
     var minute = document.getElementById("minute");
     var second = document.getElementById("second");
-    clock(hour,minute,second,8,24,240,240,100);
+    clock(hour,minute,second,8,24,240,240,100,true);
     
     var hour_2 = document.getElementById("hour_2");
     var minute_2 = document.getElementById("minute_2");
     var second_2 = document.getElementById("second_2");
-    clock(hour_2,minute_2,second_2,8,24,240,240,100);
+    clock(hour_2,minute_2,second_2,8,24,240,240,100,true);
+
+    var hour_3 = document.getElementById("hour_3");
+    var minute_3 = document.getElementById("minute_3");
+    var second_3 = document.getElementById("second_3");
+    clock(hour_3,minute_3,second_3,8,24,240,240,100);
 }
 
 // 时钟函数 
@@ -19,10 +24,14 @@ window.onload = function () {
  * 分针一圈多少分钟
  * 秒针一圈多少秒
  * 秒针移动频率（ms)
+ * close:true为停止时钟
  */
-function clock (hour,minute,second,timeZone,cycle_h,cycle_m,cycle_s,num) {
+function clock (hour,minute,second,timeZone,cycle_h,cycle_m,cycle_s,num,close) {
+    var timer = null;
+    var close = close;
+    clearInterval(timer);
     // 定时器
-    setInterval(clock_fun,num);
+    var timer = setInterval(clock_fun,num);
     // 定时器函数
     function clock_fun () {
         var date = new Date();
@@ -34,6 +43,9 @@ function clock (hour,minute,second,timeZone,cycle_h,cycle_m,cycle_s,num) {
         hour.style.transform = 'rotate('+ tow_cn_hour * (360/cycle_h)+'deg)';
         minute.style.transform = 'rotate('+ tow_min * (360/cycle_m) +'deg)';
         second.style.transform = 'rotate('+ tow_s * (360/cycle_s) +'deg)';
-
+        if (close == true) {
+            clearInterval(timer);
+        }
     }
+    
 }
